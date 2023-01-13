5e1b17dc8987d39fd572c92f5b7bf65afd93cf5f
1012068860@qq.com <1012068860@qq.com>
2023-01-13 12:32:50 +0800

    完善测试类

--- mapstruct/src/test/java/com/wmq/mapstruct/MapstructTest.java
+++ mapstruct/src/test/java/com/wmq/mapstruct/MapstructTest.java
@@ -7,7 +7,7 @@ import com.wmq.mapstruct.beans.dto.PartDTO;
 import com.wmq.mapstruct.beans.vo.CarVO;
 import com.wmq.mapstruct.beans.vo.VehicleVO;
 import com.wmq.mapstruct.convert.CarConvert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
